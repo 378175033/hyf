2d9e2b60440affbb8df7e0ba168836607959a357
378175033 <15708432599@163.com>
2018-11-03 23:47:23 +0800

    save

--- assets/Script/welcome.js
+++ assets/Script/welcome.js
@@ -11,31 +11,21 @@
 cc.Class({
     extends: cc.Component,
 
-    properties: {
-        foo: {
-            // ATTRIBUTES:
-            default: null,        // The default value will be used only when the component attaching
-                                  // to a node for the first time
-            type: cc.SpriteFrame, // optional, default is typeof default
-            serializable: true,   // optional, default is true
-        },
-        bar: {
-            get () {
-                return this._bar;
-            },
-            set (value) {
-                this._bar = value;
-            }
-        },
-    },
+    properties: {},
 
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
-
-    start () {
-
+    onLoad: function () {
+        this.node.on('mousedown', function ( event ) {
+            cc.director.loadScene("menu");
+        });
     },
+
+    start: function () {
+        // setTimeout(function () {
+        //     cc.director.loadScene("menu");
+        // }.bind(this), 5000);
+    }
 
     // update (dt) {},
 });
